575b6f27ce21cf473f4a708696f3308ba99d5f98
Nick Aiello <naiello@nd.edu>
2016-09-26 14:05:10 -0400

    Added skeleton source files

---
+++ ftpclient.c
@@ -0,0 +1,8 @@
+/* Simple FTP Client
+ * Authors: Nick Aiello / Rosalyn Tan
+ * Description: TODO
+ */
+
+int main(int argc, char **argv)
+{
+}
---
+++ ftpserver.c
@@ -0,0 +1,8 @@
+/* Simple FTP Server
+ * Authors: Rosalyn Tan / Nick Aiello
+ * Description: TODO
+ */
+
+int main(int argc, char **argv)
+{
+}
